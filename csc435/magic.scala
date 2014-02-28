f657535fd8cc18bf2206a996e87d84680f4e22cd
Jack Christiansen <christj6@tcnj.edu>
2014-02-28 14:41:43 -0500

    Update magic.scala

--- csc435/magic.scala
+++ csc435/magic.scala
@@ -23,6 +23,32 @@ object magic {
 		        }
 		  }
 
+		  def findFirstDuplicate : Int = 
+		  {
+	        for (x <- 0 to (n*n))
+	        {
+	        	var sightings = 0
+	        	for( i <- 0 to (n-1))
+		        {
+		            for( j <- 0 to (n-1))
+		            {
+		                if (main(i)(j) == x)
+		                {
+		                	sightings = sightings + 1
+		                }
+
+		                if (sightings > 1)
+		                {
+		                	// main(i)(j) has a duplicate
+		                	return main(i)(j)
+		                }
+		             }
+		        }
+	        }
+
+	        return 0
+		  }
+
 	  def solved : Boolean = 
 	  {
 	  	var rowSum = 0
@@ -85,17 +111,46 @@ object magic {
 	        return true
 	  }
 
-		 for( i <- 0 to (n*n-1))
-		 {
-		 	//matrix(i)(j) = 0
-		 }
+
+	  var x = 1
+
+		 for( i <- 0 to (n-1))
+        {
+            for( j <- 0 to (n-1))
+            {
+            	main(i)(j) = x
+            	x = x + 1
+            }
+        }
+
+        while (!solved)
+        {
+        	for (a <- 0 to n-1)
+        	{
+        		for (b <- 0 to n-1)
+	        	{
+	        		for (c <- 0 to n-1)
+		        	{
+		        		
+		        		for (d <- 0 to n-1)
+			        	{
+			        		var temp = main(a)(b)
+        					main(a)(b) = main(c)(d)
+        					main(c)(d) = temp
+			        	}
+		        	}
+	        	}
+        	}
+        	
+        }
 
 
 		//Console.print(theSum)
 
 
 		printBoard(main, n)
-		Console.print(solved)
+		//Console.print(solved)
+		//Console.print(duplicates)
 		return main
 	}
 
