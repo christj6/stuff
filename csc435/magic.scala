bb51b50c584605d23cd336ef512b52e21ebef52e
Jack Christiansen <christj6@tcnj.edu>
2014-02-28 13:17:22 -0500

    Update magic.scala

--- csc435/magic.scala
+++ csc435/magic.scala
@@ -2,25 +2,85 @@ object magic {
 
 	def squareOOP (n: Int) : Array[Array[Int]] =
 	{
-		var main = Array.ofDim[Int](n,n)
+		//var main = Array.ofDim[Int](n,n)
 
-		val sum = (n*(n*n + 1))/2
+		var main = Array(Array(2, 7, 6), Array(9, 5, 1), Array(4, 3, 8)) // test solved method
 
-		 def clearMatrix
+		val theSum = (n*(n*n + 1))/2
+
+		 def revertMatrix(point: Int)
 		  {
-		         // print sudoku board
+		         // might cut this
 		        for( i <- 0 to (n-1))
 		        {
 		            for( j <- 0 to (n-1))
 		            {
-		                matrix(i)(j) = 0
+		                if (main(i)(j) > point)
+		                {
+		                	main(i)(j) = 0
+		                }
 		             }
 		        }
 		  }
 
-		//Console.print(sum)
+	  def solved : Boolean = 
+	  {
+	  	var rowSum = 0
+	  	var columnSum = 0
+	  	var diagonalSumA = 0
+	  	var diagonalSumB = 0
+  		 // 
+	        for( i <- 0 to (n-1))
+	        {
+	        	rowSum = 0
+	        	columnSum = 0
+
+	            for( j <- 0 to (n-1))
+	            {
+	            	rowSum = rowSum + main(i)(j)
+	            	columnSum = columnSum + main(j)(i)
+
+	            	if (i == j)
+	            	{
+	            		diagonalSumA = diagonalSumA + main(i)(j)
+	            	}
+
+	            	if (i + j == n)
+	            	{
+	            		diagonalSumB = diagonalSumB + main(i)(j)
+	            	}
+	            }
+
+	            if (rowSum != theSum)
+	            {
+	            	return false
+	            }
+
+	            if (columnSum != theSum)
+	            {
+	            	return false
+	            }
+	        }
+
+	        if (diagonalSumA != theSum)
+            {
+            	return false
+            }
+
+	        return true
+	  }
+
+		 for( i <- 0 to (n*n-1))
+		 {
+		 	//matrix(i)(j) = 0
+		 }
+
+
+		//Console.print(theSum)
+
 
 		printBoard(main, n)
+		Console.print(solved)
 		return main
 	}
 
