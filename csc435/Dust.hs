7f8bcd78111e3b555a097652c4403380ee34174b
Jack Christiansen <christj6@tcnj.edu>
2014-03-19 09:12:39 -0400

    Update Dust.hs

--- csc435/Dust.hs
+++ csc435/Dust.hs
@@ -1,12 +1,6 @@
 
 
-module Dust 
-(
-	main
-,	adds
-,	construct
-
-) where
+module Dust where
 
 import Data.Array.IO 
 import Data.Array.IArray
@@ -26,24 +20,4 @@ randomInt :: Int -> Int
 randomInt n = unsafePerformIO (getStdRandom (randomR (0, n-1)))
 
 
---printArray :: ( Array (Int,Int) Int ) -> String
---printArray arr = unlines [unwords [show (arr ! (x, y)) | x <- [0..5]] | y <- [0..5]]
-
-printBoard :: [((Int,Int),Int)] -> String
-printBoard arr = "x"
-
-
-
-
-showTable :: ( Array (Int,Int) Int ) -> String
-showTable arr = 
-  unlines $ map (unwords . map (show . (arr !))) indices
-  where indices = [[(x, y) | x <- [0..8]] | y <- [0..8]]
-        ((0, 0), (8, 8)) = bounds arr
-
-
-
---test = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-
---board = [((x, y), 0) | x <- [0..8], y <- [0..8]]
 
