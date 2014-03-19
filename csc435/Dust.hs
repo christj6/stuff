3b7b63fa7c1ae74e8a2961871ab006cf01afd60f
Jack Christiansen <christj6@tcnj.edu>
2014-03-19 15:40:11 -0400

    Update Dust.hs

--- csc435/Dust.hs
+++ csc435/Dust.hs
@@ -17,7 +17,7 @@ construct n = [((x, y), -1) | x <- [0..n-1], y <- [0..n-1]]
 randomInt :: Int -> Int
 randomInt n = unsafePerformIO (getStdRandom (randomR (0, n-1)))
 
--- let cell = (construct 5)!!4
+-- let cell = (construct 5)!!4 where 5 and 4 are any old numbers (5 determines length of board, 4 is location in board)
 -- so cell is a tuple ((x, y), val)
 -- x = fst (fst cell)
 -- y = snd (fst cell)
