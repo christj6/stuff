0df421e045d3a5e8b8382d45314801d73db2cd04
Jack Christiansen <christj6@tcnj.edu>
2014-03-20 09:09:23 -0400

    Update Dust.hs

--- csc435/Dust.hs
+++ csc435/Dust.hs
@@ -17,9 +17,23 @@ adds x y = x + y
 construct :: Int -> [((Int,Int),Int)]
 construct n = [((x, y), -1) | x <- [0..n-1], y <- [0..n-1]]
 
-printBoard :: [((Int,Int),Int)] -> IO()
-printBoard arr = do
-	putStr "x\n"
+--change :: Int -> Int -> Int -> [((Int,Int),Int)] -> [((Int,Int),Int)]
+--change x y val arr = do
+
+	--where
+		--let cell = arr!!
+		--x = fst (fst cell)
+		--y = snd (fst cell)
+
+printBoard :: Int -> Int -> [((Int,Int),Int)] -> IO()
+printBoard n m arr = do
+	let cell = (arr !! m)
+	let x = fst (fst cell)
+	let val = snd cell
+	if x == (n-1)
+		then putStr "value" 
+		else putStrLn "value"
+	printBoard n (m+1) arr
 
 randomInt :: Int -> Int
 randomInt n = unsafePerformIO (getStdRandom (randomR (0, n-1)))
@@ -31,4 +45,4 @@ randomInt n = unsafePerformIO (getStdRandom (randomR (0, n-1)))
 -- val = snd cell
 
 -- replicateM_ 10 $ putStrLn "a string"
--- that prints something 10 times in a row
+-- that prints something 10 times in a row; remember to import Control.Monad
