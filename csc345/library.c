3178447995167a5c441b3b9220e60c587c4d7a97
Jack Christiansen <christj6@tcnj.edu>
2014-03-22 21:24:14 -0400

    Update library.c
    added constants

--- csc345/library.c
+++ csc345/library.c
@@ -12,6 +12,13 @@
 
 #include <pthread.h>
 
+#define DAYS 30 // the number of days the simulation will cover
+#define HOURS 16 // 8am - 12am, the max number of hours that can be occupied on a given day
+#define MAX_SEATING 12 // the maximum number of seats that a room in the set of rooms can offer
+
+#define ROOMS 26 // number of rooms
+#define USERS 10 // number of users operating at a given time (may be changed to a variable)
+
 
 typedef struct
 {
@@ -19,10 +26,7 @@ typedef struct
 	int seating; /* number of people who can fit in the room */
 
 	pthread_mutex_t available;
-
-	/* Each room will get its own 2d array of seating values (date/time),
-	when a user requests a room at a given time, if the number isn't 0,
-	1 will be subtracted from the number. */
+	
 
 	/* Monday-Thursday, 8:00 a.m. – 12:00 a.m. (16 hours)
 	Friday, 8:00 a.m. – 6:00 p.m. (10 hours)
@@ -31,19 +35,24 @@ typedef struct
 
 	// Mon, Tues, Wed, Thurs, Fri, Sat, Sun (7)
 	// 8 9 10 11 12 1 2 3 4 5 6 7 8 9 10 11 (not 12, it's closed at that point) (16)
-	int seats [7][16]; /* first index is day of the week, 2nd is the hour */
-
+	
+	
+	int seats [DAYS][HOURS][MAX_SEATING]; 
+	/* first index is day of the week, 2nd is the hour
+	 30 refers to the number of days
+	 mod 7 = 0, monday
+	mod 7 = 1, tues etc. */
 	
 	/* For specialPurpose: 0 = none, 1 = storage, 2 = group listening,
 	3 = group viewing, 4 = graduate students */
 	int specialPurpose;
 } Room;
 
-Room studyRooms[26];
+Room studyRooms[ROOMS];
 
-int rmNumbers[26] = {109, 110, 111, 202, 205, 220, 224, 225, 226, 228, 301, 308, 309, 310, 311, 315, 316, 317, 319, 404, 406, 411, 412, 413, 414, 415};
+int rmNumbers[ROOMS] = {109, 110, 111, 202, 205, 220, 224, 225, 226, 228, 301, 308, 309, 310, 311, 315, 316, 317, 319, 404, 406, 411, 412, 413, 414, 415};
 
-int stNumbers [26] = {4, 6, 4, 6, 6, 12, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 4, 8, 6, 6, 12, 6, 6};
+int stNumbers [ROOMS] = {4, 6, 4, 6, 6, 12, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 4, 8, 6, 6, 12, 6, 6};
 
 typedef struct
 {
@@ -64,7 +73,7 @@ void *calendarize (void *arg)
  	//printf("%d\n", user->roomRequested);
 
 	int count;
-	for (count = 0; count < 26; count++)
+	for (count = 0; count < ROOMS; count++)
 	{
 		if (studyRooms[count].roomNumber == user->roomRequested)
 		{
@@ -97,13 +106,29 @@ int main()
 	
 
 	int i;
-	for (i = 0; i < 26; i++)
+	for (i = 0; i < ROOMS; i++)
 	{
 		studyRooms[i].roomNumber = rmNumbers[i];
 		studyRooms[i].seating = stNumbers[i];
+		
+		int a;
+		int b;
+		int c;
+	
+		for (a = 0; a < DAYS; a++)
+		{
+			for (b = 0; b < HOURS; b++)
+			{
+				for (c = 0; c < MAX_SEATING; c++)
+				{
+					studyRooms[i].seats[a][b][c] = 0;
+				}
+			}
+		}
+		// end of loops
 	}
 	
-	for (i = 0; i < 26; i++)
+	for (i = 0; i < ROOMS; i++)
 	{
 		//printf("%s", "room: ");
 		//printf("%d\n", studyRooms[i].roomNumber);
@@ -112,8 +137,8 @@ int main()
 	// at this point, all rooms are scanned inside the simulation
 	
 	// users: User ID, email, room requested, hours requested, willing to sub
-	int numberOfUsers = 10;
-	User users[numberOfUsers]; // for testing purposes, right now it has 10 users
+
+	User users[USERS]; // for testing purposes, right now it has 10 users
 	
 	FILE *textFile;
 	textFile = fopen("users.txt", "r");
@@ -134,19 +159,19 @@ int main()
 	// at this point, all "users" are scanned inside the simulation
 	// ------------------------------------------------------------------
 	
-	pthread_t threads[numberOfUsers];
+	pthread_t threads[USERS];
 	
-	for (i = 0; i < numberOfUsers; i++)
+	for (i = 0; i < USERS; i++)
 	{
 		pthread_create(&threads[i], NULL, calendarize, (void *) &users[i]);
 	}
 	
-	for (i = 0; i < numberOfUsers; i++)
+	for (i = 0; i < USERS; i++)
 	{
 		pthread_join(threads[i], NULL);
 	}
 
-	for (i = 0; i < 26; i++)
+	for (i = 0; i < ROOMS; i++)
 	{
 		//printf("%s", "room: ");
 		//printf("%d\n", studyRooms[i].roomNumber);
