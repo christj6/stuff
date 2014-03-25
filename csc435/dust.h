db3b2ea4edd81db43a3ad81cf4dbd40c8b844a7b
Jack Christiansen <christj6@tcnj.edu>
2014-03-25 01:11:24 -0400

    Update dust.h

--- csc435/dust.h
+++ csc435/dust.h
@@ -1,10 +1,6 @@
 #ifndef DUST_H_   /* Include guard */
 #define DUST_H_
 
-//int game;
-//int board[int][int];
-//int mineXcoord[int];
-//int mineYcoord[int];
 int mineHit(int, int);
 int returnValue(int, int);
 int winCheck();
@@ -13,7 +9,7 @@ void exploreLeft(int, int);
 void exploreUp(int, int);
 void exploreRight(int, int);
 void exploreDown(int, int);
+void generateMines();
 void printBoard();
-//int main(int);
 
 #endif
