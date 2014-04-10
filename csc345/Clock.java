f7e193dbefcc7e3001d63a3a46fc3ed6d595d312
Jack Christiansen <christj6@tcnj.edu>
2014-04-10 16:35:25 -0400

    Update Clock.java

--- csc345/Clock.java
+++ csc345/Clock.java
@@ -7,8 +7,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 // Used content from: http://stackoverflow.com/questions/1410741/want-to-invoke-a-linux-shell-command-from-java
+// And a bit from: http://stackoverflow.com/questions/9993873/java-executing-linux-command
 
 public class Clock
 {
@@ -17,23 +19,41 @@ public class Clock
 	{
 		long start;
 		long end;
-
-		start = System.nanoTime();
-
-		// do something here
-
 		List<String> commands = new ArrayList<String>();
+		String directory = "/Users/Jack/Documents/GitHub/stuff/csc345";
+
+		
 		commands.add("ls");
+
 
 		// Run macro on target
 		ProcessBuilder pb = new ProcessBuilder(commands);
-	        pb.directory(new File("/Users/Jack/Documents/GitHub/stuff/csc345"));
+	        pb.directory(new File(directory));
 	        pb.redirectErrorStream(true);
+	
+	        start = System.nanoTime();
 	        Process process = pb.start();
+	        end = System.nanoTime(); // measured in nanoseconds
 	
 	        // read output
-	        StringBuilder out = new StringBuilder();
-	        BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+	        output(new InputStreamReader(process.getInputStream()));
+	
+	        
+	
+			System.out.println("\nTime " + (end - start)/1000000000f + " seconds.");
+		}
+	
+		public void shell()
+		{
+			Scanner scan = new Scanner(System.in);
+	
+		}
+	
+		public static void output(InputStreamReader stream) throws IOException
+		{
+			StringBuilder out = new StringBuilder();
+	        //BufferedReader br = new BufferedReader(stream);
+	        BufferedReader br = new BufferedReader(stream);
 	        String line = null, previous = null;
 	        while ((line = br.readLine()) != null)
 	        {
@@ -44,13 +64,5 @@ public class Clock
 	                System.out.println(line);
 	            }
 	        }
-            
-
-		
-		// stop doing that thing here
-
-		end = System.nanoTime(); // measured in nanoseconds
-
-		System.out.println("\nTime " + (end - start)/1000000000f + " seconds.");
 	}
 }
