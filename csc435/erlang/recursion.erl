79c51ccc81cbebb3236a04e77d689e00e8b9ee60
Jack Christiansen <christj6@tcnj.edu>
2014-04-15 22:34:11 -0400

    Update recursion.erl

--- csc435/erlang/recursion.erl
+++ csc435/erlang/recursion.erl
@@ -1,5 +1,5 @@
 -module(recursion).
--export([write/1,pascal/1]).
+-export([write/1,pascal/1,listAdd/2,pascalString/1]).
 
 
 write(Data) -> {ok, IODevice} = file:open("Pascal.txt", [write]), file:write(IODevice, Data), file:close(IODevice).
@@ -7,10 +7,26 @@ write(Data) -> {ok, IODevice} = file:open("Pascal.txt", [write]), file:write(IOD
 pascal(Int) ->
 	if
 		Int == 1 ->
-			" 1 ";
-		Int == 2 ->
-			pascal(Int-1) ++ " 1 1 ";
-		Int == 3 ->
-			pascal(Int-1) ++ " 1 2 1 "
+			[[1]];
+		Int > 1 ->
+			Tail = pascal(Int-1),
+			Head = hd(Tail),
+    		[listAdd([0]++Head, Head++[0])] ++ Tail
+	end.
 
+% adds corresponding elements of 2 lists together, compiles corresponding sums in one list
+listAdd(List1, List2) ->
+	if
+		length(List1) == 1 ->
+			[hd(List1) + hd(List2)];
+		length(List1) /= 0 ->
+			[hd(List1) + hd(List2)]++listAdd(tl(List1),tl(List2))
+	end.
+
+pascalString(List) -> 
+	if
+		tl(List) == [] ->
+			" ";
+		tl(List) /= [] ->
+			(hd(List)++" ")++pascalString(tl(List))
 	end.
