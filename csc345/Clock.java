775a32a75b54f52b5ad768a3c44c633971651f51
Jack Christiansen <christj6@tcnj.edu>
2014-04-17 21:01:25 -0400

    simplified

--- csc345/Clock.java
+++ csc345/Clock.java
@@ -23,76 +23,59 @@ public class Clock
 		String sourceDirectory = "/Users/Jack/Documents/GitHub/stuff/csc345/clock";
 		String destinationDirectory = "H:/"; // set this to the appropriate directory if you are using a flash drive
 		//String directory = "/cygwin/h";
-
-		int write = 0; // If write is 1, the timed section involves writing a file. If it's 0, it involves reading/accessing data (via UNIX command).
-
-		if (write == 0)
-		{
-			System.out.println("Read mode ON -- make sure valid UNIX command is in ProcessBuilder.");
-		}
-
-		if (write == 1)
-		{
-			System.out.println("Write mode ON.");
-		}
      
-        for (int i = 0; i < trials; i++)
-        {
-        	//ProcessBuilder pb = new ProcessBuilder("cat","file" + i + ".txt"); // Performs read of file (cat with single argument)
-        	ProcessBuilder pb = new ProcessBuilder("cp","file" + i + ".txt",destinationDirectory); // copy file to directory
-
-        	pb.directory(new File(sourceDirectory));
-        	// Timed section begins here:
-	        if (write == 0)
+	        for (int i = 0; i < trials; i++)
 	        {
-	        	start = System.nanoTime();
-	        	Process process = pb.start();  
-	        	end = System.nanoTime();
-	        }
-
-	        if (write == 1)
-	        {
-	        	start = System.nanoTime();
-	        	//writeFile("file" + i + ".txt","0123456789"); // first argument is the filename, second is the data in the string
-	        	end = System.nanoTime();
-	        }
-	        // Timed section ends here.
-
-	        //output(process); // output the text -- mainly for debug purposes (making sure the process executes the way you intended)
-
+	        	//ProcessBuilder pb = new ProcessBuilder("cp","file" + i + ".txt",destinationDirectory); // copy file to directory
+	        	//ProcessBuilder pb = new ProcessBuilder("cat","file" + i + ".txt"); // Performs read of file (cat with single argument)
+	        	ProcessBuilder pb = new ProcessBuilder("rm", destinationDirectory + "file" + i + ".txt"); // delete file from flash drive
+	
+	        	pb.directory(new File(sourceDirectory));
+	        	
+	        	// Timed section begins here:
+		        if (write == 0)
+		        {
+		        	start = System.nanoTime();
+		        	Process process = pb.start();  
+		        	end = System.nanoTime();
+		        }
+		        // Timed section ends here.
+	
+		        //output(process); // output the text -- mainly for debug purposes (making sure the process executes the way you intended)
+	
 			//System.out.println("\nTime " + (end - start)/1000000000f + " seconds.");
 			data[i] = (end - start)/1000000000f;
 			outputReadyData += data[i];
 			outputReadyData += ",";
 			//outputReadyData += "\r\n";
 			//System.out.println("Time: " + data[i] + " seconds.");
-        }
+	        }
 
-    	writeFile("values.csv", outputReadyData);
+    		writeFile("values.csv", outputReadyData);
 	}
 
 	public static void writeFile(String filename, String data) throws IOException
 	{
 		FileWriter fileWriter = new FileWriter(filename);
-        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-        bufferedWriter.write(data);
-        bufferedWriter.close();
+	        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+	        bufferedWriter.write(data);
+	        bufferedWriter.close();
 	}
 
 	// Takes in process, outputs the result of it to the screen
 	public static void output(Process process) throws IOException
 	{
 		StringBuilder out = new StringBuilder();
-        BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
-        String line = null, previous = null;
-        while ((line = br.readLine()) != null)
-        {
-        	if (!line.equals(previous)) 
-        	{
-                previous = line;
-                out.append(line).append('\n');
-                System.out.println(line);
+	        BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+	        String line = null, previous = null;
+	        while ((line = br.readLine()) != null)
+	        {
+	        	if (!line.equals(previous)) 
+	        	{
+	                previous = line;
+	                out.append(line).append('\n');
+	                System.out.println(line);
+		        }
 	        }
-        }
 	}
 }
