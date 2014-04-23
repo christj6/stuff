4d3479419aa0b4fed48684374ed4199063a7e7f2
Jack Christiansen <christj6@tcnj.edu>
2014-04-23 00:21:37 -0400

    Update people.pl

--- csc435/prolog/people.pl
+++ csc435/prolog/people.pl
@@ -1,6 +1,26 @@
 
 
+
 female(mary).
 female(alex).
 male(joe).
 male(carl).
+
+% our assumptions
+married(mary,carl).
+married(carl,mary).
+job(mary).
+biologyMajor(joe).
+watchingTV(joe).
+sleepless(alex).
+
+happy(X) :-
+	watchingTV(X);
+	married(X,Y), \+ sleepless(X).
+
+sleepless(X) :-
+	biologyMajor(X), job(X);
+	biologyMajor(X), kids(X);
+	kids(X), job(X);
+	biologyMajor(X), job(X), kids(X).
+
