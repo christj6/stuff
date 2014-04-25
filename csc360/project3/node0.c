4dd384e502e2264c6fadc4184119d938d09a2a64
Jack Christiansen <christj6@tcnj.edu>
2014-04-25 17:14:27 -0400

    formatting

--- csc360/project3/node0.c
+++ csc360/project3/node0.c
@@ -1,62 +1,52 @@
 #include <stdio.h>
-extern struct rtpkt {
-int sourceid; /* id of sending router sending this pkt */ int destid; /* id of router to which pkt being sent
-(must be an immediate neighbor) */ int mincost[4]; /* min cost to node 0 ... 3 */
+
+extern struct rtpkt 
+{
+	int sourceid; /* id of sending router sending this pkt */ 
+	int destid; /* id of router to which pkt being sent (must be an immediate neighbor) */ 
+	int mincost[4]; /* min cost to node 0 ... 3 */
 };
-extern int TRACE; extern int YES; extern int NO;
-struct distance_table {
-int costs[4][4]; } dt0;
+
+extern int TRACE; 
+extern int YES; 
+extern int NO;
+
+struct distance_table 
+{
+	int costs[4][4]; 
+} dt0;
+
 /* students to write the following two routines, and maybe some others */
-void rtinit0() {
+void rtinit0() 
+{
+
 }
-void rtupdate0(rcvdpkt) struct rtpkt *rcvdpkt;
-{ }
+
+
+void rtupdate0(rcvdpkt) 
+struct rtpkt *rcvdpkt;
+{ 
+
+}
+
 printdt0(dtptr)
 struct distance_table *dtptr;
 {
+	printf(" via \n");
+	printf(" D0 | 1 2 3 \n");
+	printf(" ----|-----------------\n");
+	printf(" 1| %3d %3d %3d\n",dtptr->costs[1][1],
+	dtptr->costs[1][2],dtptr->costs[1][3]); printf("dest 2| %3d %3d %3d\n",dtptr->costs[2][1],
+	dtptr->costs[2][2],dtptr->costs[2][3]);
+	printf(" 3| %3d %3d %3d\n",dtptr->costs[3][1],
+	dtptr->costs[3][2],dtptr->costs[3][3]);
 }
-printf(" via
-printf(" D0 | 1 2
-printf(" ----|-----------------\n");
-printf(" 1| %3d %3d %3d\n",dtptr->costs[1][1],
-dtptr->costs[1][2],dtptr->costs[1][3]); printf("dest 2| %3d %3d %3d\n",dtptr->costs[2][1],
-dtptr->costs[2][2],dtptr->costs[2][3]);
-printf(" 3| %3d %3d %3d\n",dtptr->costs[3][1],
-dtptr->costs[3][2],dtptr->costs[3][3]);
-linkhandler0(linkid, newcost) int linkid, newcost;
+
+linkhandler0(linkid, newcost) 
+int linkid, newcost;
 /* called when cost from 0 to linkid changes from current value to newcost*/
 /* You can leave this routine empty if you're an undergrad. If you want */
 /* to use this routine, you'll need to change the value of the LINKCHANGE */
 /* constant definition in prog3.c from 0 to 1 */
-{ }
-node1.c code
-#include <stdio.h>
-extern struct rtpkt { int sourceid; /* int destid; /*
-id of sending router sending this pkt */ id of router to which pkt being sent (must be an immediate neighbor) */
-int mincost[4]; /* min cost to node 0 ... 3 */ };
-extern int TRACE; extern int YES; extern int NO;
-int connectcosts1[4] = { 1, 0, 1, 999 };
-\n"); 3 \n");
-
-struct distance_table {
-int costs[4][4]; } dt1;
-/* students to write the following two routines, and maybe some others */
-rtinit1() {
+{ 
 }
-rtupdate1(rcvdpkt) struct rtpkt *rcvdpkt;
-{ }
-printdt1(dtptr)
-struct distance_table *dtptr;
-{
-printf(" via \n");
-printf(" D1 | 0 2 \n");
-printf(" ----|-----------\n");
-printf(" 0| %3d %3d\n",dtptr->costs[0][0], dtptr->costs[0][2]); printf("dest 2| %3d %3d\n",dtptr->costs[2][0], dtptr->costs[2][2]); printf(" 3| %3d %3d\n",dtptr->costs[3][0], dtptr->costs[3][2]);
-}
-linkhandler1(linkid, newcost)
-int linkid, newcost;
-/* called when cost from 1 to linkid changes from current value to newcost*/
-/* You can leave this routine empty if you're an undergrad. If you want */
-/* to use this routine, you'll need to change the value of the LINKCHANGE */
-/* constant definition in prog3.c from 0 to 1 */
-{ }
