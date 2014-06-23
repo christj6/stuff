56a1e8519264fa9b5597fb8552d3331687051735
Jack Christiansen <christj6@tcnj.edu>
2014-06-23 09:25:57 -0400

    reads from excel file

--- google.rb
+++ google.rb
@@ -9,38 +9,46 @@ agent = Mechanize.new
 workbook = RubyXL::Parser.parse("EEC PARTS.xlsx")
 worksheet = workbook[0]
 
+begin
+	file = File.open("runners.html", "w")
+	for i in 0..23
+		_part = "site:eecontrols.com filetype:pdf " + worksheet[i][0].value.to_s
+		
+		agent.get('http://www.google.com') do |page|
+			search_result = page.form_with(:action => '/search') do |search|
+				search.q = _part
+			end.submit
+			
+			page = Nokogiri::HTML(open(search_result.uri.to_s))
+			results = page.css('div h3 a')
+
+			link = "www.google.com" + (results[0].to_s).gsub('<a href="', "")
+			link = link.split('>').first
+			link = link.split('"').first # if google ever put double quotes inside their URLs, this string parsing method would cause issues
+			link = "http://" + link # now link looks like: http://www.google.com/url?q=http://www.eecontrols.com/documents/Page170.pdf&amp;sa=U&amp;ei=MRqoU6iCDuTK8wG4wIGACg&amp;ved=0CBQQFjAA&amp;usg=AFQjCNG6JKe0-JvfV-PK86YcF4tvhusKyQ
+			#puts link
+			
+			fetch = Nokogiri::HTML(open(link.to_s))
+			actualLink = fetch.css('div a')[0].to_s
+			
+			actualLink = actualLink.gsub('<a href="', "")
+			actualLink = actualLink.split('">').first
+			
+			#puts actualLink # awesome, this returns what looks like: http://www.eecontrols.com/documents/Page170.pdf
+			file.write(actualLink + "\n")
+		end
+		
+	end 
+	rescue IOError => e
+		#some error occur, directory not writable etc.
+	ensure
+		file.close unless file == nil
+end
+
 #_part = "site:www.eecontrols.com s4-405-m1" # this one returns the right url as the first one
 _part = "site:www.eecontrols.com bfk-1s"
 
-agent.get('http://www.google.com') do |page|
-	search_result = page.form_with(:action => '/search') do |search|
-		search.q = _part
-	end.submit # end.submit doesn't work, nil:NilClass?
-	
-	page = Nokogiri::HTML(open(search_result.uri.to_s))
-	results = page.css('div h3 a')
 
-	puts "You searched for: "
-
-	link = "www.google.com" + (results[0].to_s).gsub('<a href="', "")
-	link = link.split('>').first
-	link = link.split('"').first # if google ever put double quotes inside their URLs, this string parsing method would cause issues
-	link = "http://" + link # now link looks like: http://www.google.com/url?q=http://www.eecontrols.com/documents/Page170.pdf&amp;sa=U&amp;ei=MRqoU6iCDuTK8wG4wIGACg&amp;ved=0CBQQFjAA&amp;usg=AFQjCNG6JKe0-JvfV-PK86YcF4tvhusKyQ
-	#puts link
-	
-	fetch = Nokogiri::HTML(open(link.to_s))
-	actualLink = fetch.css('div a')[0].to_s
-	
-	actualLink = actualLink.gsub('<a href="', "")
-	actualLink = actualLink.split('">').first
-	
-	puts actualLink # awesome, this returns what looks like: http://www.eecontrols.com/documents/Page170.pdf
-	
-	for i in 0..2
-		# do stuff with excel file here
-	end
-	
-end
 
 =begin
 root = TkRoot.new { title "Hello, World!" }
