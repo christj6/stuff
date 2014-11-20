2997082f7e26f19917383654caf59407c8942b33
Jack Christiansen <christj6@tcnj.edu>
2014-11-20 14:42:01 -0500

    Update Search.java

--- csc320/Search.java
+++ csc320/Search.java
@@ -16,7 +16,7 @@ public class Search {
 		{
 			String line = br.readLine();
 			int documentNumber = 0;
-			int position = 0;
+			//int position = 0;
 
 			while (line != null)
 			{
@@ -29,7 +29,7 @@ public class Search {
 					line = line.replaceAll("[^0-9]", ""); // filter out spaces and nonalphanumeric characters
 					documentNumber = Integer.parseInt(line); // since the tccorpus.txt file has the documents in order, it's not necessary to parse the number each time, but this gives the ability to parse a text file with out of order documents
 					//System.out.println(documentNumber);
-					position = 0;
+					//position = 0;
 				}
 				else
 				{
@@ -55,7 +55,7 @@ public class Search {
 						//Tuple x = new Tuple(documentNumber, position);
 						entry.add(documentNumber);
 
-						position++;
+						//position++;
 					}
 				}
 
@@ -69,7 +69,13 @@ public class Search {
 
 
 		// word -- docId, term frequency
-
+		// referenced this for printing values -- http://stackoverflow.com/questions/5920135/printing-hashmap-in-java
+		// this loop prints the term, followed by the appearances the term makes in documents in the collection.
+		// for example --
+		// women: 2735 2735 2823 2823 2823 2823 2823 3022 3022 3022 3022
+		// we want this to instead look like --
+		// women: (2735, 2) (2823, 5) (3022, 4) 
+		/*
 		for (String name: index.keySet())
 		{
             String key = name.toString();
@@ -87,6 +93,7 @@ public class Search {
 
         	System.out.println("");
 		} 
+		*/
 
 	}
 
